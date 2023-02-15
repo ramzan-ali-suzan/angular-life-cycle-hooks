3a0f3f997fa18813390d7a05ba987e972ad6a064
Muhammad Ramzan Ali Suzan <ramzan.ali.suzan@gmail.com>
2023-02-15 14:34:51 +0600

    Added ngAfterContentInit examples

--- src/app/components/child/child.component.ts
+++ src/app/components/child/child.component.ts
@@ -1,16 +1,20 @@
-import { Component, DoCheck, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { AfterContentInit, Component, ContentChild, DoCheck, ElementRef, Input, OnChanges, OnInit, SimpleChanges, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-child',
   templateUrl: './child.component.html',
   styleUrls: ['./child.component.css']
 })
-export class ChildComponent implements OnChanges, OnInit, DoCheck {
+export class ChildComponent implements OnChanges, OnInit, DoCheck, AfterContentInit {
+
   @Input() userName = '';
   @Input() adminUser = { name: '', role: '' };
 
+  @ViewChild('wrapper') wrapper!: ElementRef;
+  @ContentChild('content') content!: ElementRef;
+
   ngOnChanges(changes: SimpleChanges): void {
-    console.log('ngChanges triggered', changes);
+    // console.log('ngChanges triggered', changes);
 
     // if (changes['userName'].isFirstChange()) {
     //   this.userName = 'Hello ' + this.userName;
@@ -25,6 +29,11 @@ export class ChildComponent implements OnChanges, OnInit, DoCheck {
   }
 
   ngDoCheck(): void {
-    console.log('ngDoCheck triggered', this.adminUser);
+    // console.log('ngDoCheck triggered', this.adminUser);
+  }
+
+  ngAfterContentInit(): void {
+    console.log('ngAfterContentInit - wrapper', this.wrapper);
+    console.log('ngAfterContentInit - content', this.content);
   }
 }
