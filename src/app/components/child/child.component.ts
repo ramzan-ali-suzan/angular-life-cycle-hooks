b10205ae1c8612741a755584fc5f00548a124333
Muhammad Ramzan Ali Suzan <ramzan.ali.suzan@gmail.com>
2023-02-15 13:40:23 +0600

    Added ngOnChanges example

--- src/app/components/child/child.component.ts
+++ src/app/components/child/child.component.ts
@@ -1,10 +1,21 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-child',
   templateUrl: './child.component.html',
   styleUrls: ['./child.component.css']
 })
-export class ChildComponent {
+export class ChildComponent implements OnChanges {
   @Input() userName = '';
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('ngChanges triggered', changes);
+
+    if (changes['userName'].isFirstChange()) {
+      this.userName = 'Hello ' + this.userName;
+      return;
+    }
+
+    this.userName = 'Hi ' + this.userName;
+  }
 }
