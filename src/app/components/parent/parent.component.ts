aa5865c427149d9dc831553339500060c792e854
Muhammad Ramzan Ali Suzan <ramzan.ali.suzan@gmail.com>
2023-02-15 12:51:40 +0600

    Add data passing between parent and child component

--- src/app/components/parent/parent.component.ts
+++ src/app/components/parent/parent.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./parent.component.css']
 })
 export class ParentComponent {
+  userName = 'Masud';
 
+  update() {
+    this.userName = 'Ramzan';
+  }
 }
