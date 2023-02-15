8fc72b14c868275cde28a2bd7052e435f817f823
Muhammad Ramzan Ali Suzan <ramzan.ali.suzan@gmail.com>
2023-02-15 13:48:38 +0600

    Added ngOnInit examples

--- src/app/components/child/child.component.ts
+++ src/app/components/child/child.component.ts
@@ -1,21 +1,25 @@
-import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-child',
   templateUrl: './child.component.html',
   styleUrls: ['./child.component.css']
 })
-export class ChildComponent implements OnChanges {
+export class ChildComponent implements OnChanges, OnInit {
   @Input() userName = '';
 
   ngOnChanges(changes: SimpleChanges): void {
-    console.log('ngChanges triggered', changes);
+    // console.log('ngChanges triggered', changes);
 
-    if (changes['userName'].isFirstChange()) {
-      this.userName = 'Hello ' + this.userName;
-      return;
-    }
+    // if (changes['userName'].isFirstChange()) {
+    //   this.userName = 'Hello ' + this.userName;
+    //   return;
+    // }
 
-    this.userName = 'Hi ' + this.userName;
+    // this.userName = 'Hi ' + this.userName;
+  }
+
+  ngOnInit(): void {
+    console.log('ngOnInit from the child component');
   }
 }
--- src/app/components/parent/parent.component.ts
+++ src/app/components/parent/parent.component.ts
@@ -1,14 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-parent',
   templateUrl: './parent.component.html',
   styleUrls: ['./parent.component.css']
 })
-export class ParentComponent {
+export class ParentComponent implements OnInit {
   userName = 'Masud';
 
   update() {
     this.userName = 'Ramzan';
+  }
+
+  ngOnInit(): void {
+    console.log('ngOnInit from the parent component');
   }
 }
